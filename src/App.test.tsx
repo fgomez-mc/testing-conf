d46b271fd0a44fb26b7ac94f3012913ef690abdc
fgomez <fgomez.sierra95@gmail.com>
2019-10-12 10:14:28 -0600

    implement and enzyme

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import { configure } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import React from "react";
+import { mount } from "enzyme";
+import App from "./App";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+configure({ adapter: new Adapter() });
+
+describe("App", () => {
+  const wrapper = mount(<App />);
+
+  it("can be mounted", () => {
+    expect(wrapper.find(".App")).toHaveLength(1);
+    expect(wrapper.find("h1").text()).toBe("Order Manager");
+  });
+  it("should contain the render panel", () => {
+    expect(wrapper.find(".OrderPanel")).toHaveLength(1);
+  });
 });
